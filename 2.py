2a1ab43b50cdbb960830532b16531dabe861165b
pavan_pulapa <ee16btech11030@iith.ac.in>
2019-02-19 21:30:39 +0530

    second commit

--- 2.py
+++ 2.py
@@ -16,7 +16,7 @@ import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 
-                  
+
 
 
 def pooling_function(img,start_length,start_width,l,w):
@@ -25,11 +25,12 @@ def pooling_function(img,start_length,start_width,l,w):
         for j in range(w):
             ans=max(ans,img[start_length+i,start_width+j])
     return ans
-    
+
+################################################################################
 def pooling(img,l,w):
     img_width=np.size(img[0,:])
     img_length=np.size(img[:,0])
-    
+
     pad_length=l-(img_length-l*int(img_length/l))
     pad_width=w-(img_width-w*int(img_width/w))
     img_padded=np.zeros((img_length+pad_length,img_width+pad_width));
@@ -41,7 +42,7 @@ def pooling(img,l,w):
         for k in range(width-1):
             output[j,k]=pooling_function(img,j*l,k*w,l,w)
     return output
-    
+
 def activation_function(x):#RELU function
     if(x<0):
         return 0
@@ -49,8 +50,8 @@ def activation_function(x):#RELU function
         return x
 
 
-        
-    
+
+
 def correlation(img,l,w,kernel):
     length=np.size(kernel[:,0,0])
     width=np.size(kernel[0,:,0])
@@ -61,39 +62,39 @@ def correlation(img,l,w,kernel):
             for k in range(width):
                 sum=sum+(img[l+j,w+k,i]*kernel[j,k,i])
     return sum
-    
+
 def convolution(img,kernel,padding,stride,bias):
     img_width=np.size(img[0,:,0])
     img_length=np.size(img[:,0,0])
     img_depth=np.size(img[0,0,:])
-    
+
     k_l=np.size(kernel[:,0,0])
     k_w=np.size(kernel[0,:,0])
     k_d=np.size(kernel[0,0,:])
     if(padding=="valid"):
-        
+
         l=int((img_length-k_l)/stride)+1
         w=int((img_width-k_w)/stride)+1
-        
+
         pad_length=k_l-(img_length-((l-1)*stride+k_l))
         pad_width=k_w-(img_width-((w-1)*stride+k_w))
-        
+
         img_padded=np.zeros((img_length+pad_length,img_width+pad_width,img_depth));
         img_padded[:-pad_length,:-pad_width,:]=img
-        
-        
+
+
         img_padded_width=np.size(img_padded[0,:,0])
         img_padded_length=np.size(img_padded[:,0,0])
-        img_padded_depth=np.size(img_padded[0,0,:]) 
-        
-        
+        img_padded_depth=np.size(img_padded[0,0,:])
+
+
         l=int((img_padded_length-k_l)/stride)+1
         w=int((img_padded_width-k_w)/stride)+1
         output=np.zeros((l,w))
         for i in range(l):
             for j in range(w):
                 output[i,j]=activation_function(correlation(img_padded,i*stride,j*stride,kernel)+bias)
-                
+
     if(padding=="same"):
         required_length=(img_length-1)*stride+k_l
         required_width=(img_width-1)*stride+k_w
@@ -103,19 +104,19 @@ def convolution(img,kernel,padding,stride,bias):
         r_w=int(required_padd_width/2)
         img_padded=np.zeros((required_length,required_width,img_depth))
         img_padded[r_l:r_l+img_length,r_w:r_w+img_width,:]=img
-     
+
         img_padded_width=np.size(img_padded[0,:,0])
         img_padded_length=np.size(img_padded[:,0,0])
-        img_padded_depth=np.size(img_padded[0,0,:]) 
-        
+        img_padded_depth=np.size(img_padded[0,0,:])
+
         l=int((img_padded_length-k_l)/stride)+1
         w=int((img_padded_width-k_w)/stride)+1
         output=np.zeros((l,w))
         for i in range(l):
             for j in range(w):
-                output[i,j]=activation_function(correlation(img_padded,i*stride,j*stride,kernel)+bias) 
-                
-                
+                output[i,j]=activation_function(correlation(img_padded,i*stride,j*stride,kernel)+bias)
+
+
     return output
 
 
@@ -131,10 +132,9 @@ for i in range(k_l):
     for j in range(k_w):
         for k in range(depth):
             kernel[i,j,k]= np.random.normal(0, 2, 1)
-                    
+
 #test=convolution(img,kernel,"valid",5,bias)
 activation_map=convolution(img,kernel,"valid",5,bias)
 pooled_output=pooling(activation_map,2,2)
 plt.imshow(pooled_output, cmap = 'gray')
 plt.show()
-              
--- q3.py
+++ q3.py
@@ -14,7 +14,7 @@ def activation_function(x):
         return 0
     else:
         return x
-
+##
 def correlation(img,l,w,kernel,d):
     length=np.size(kernel[:,0,0,d])
     width=np.size(kernel[0,:,0,d])
@@ -30,35 +30,35 @@ def convolution(img,kernel,d,padding,stride,bias):
     img_width=np.size(img[0,:,0])
     img_length=np.size(img[:,0,0])
     img_depth=np.size(img[0,0,:])
-    
+
     k_l=np.size(kernel[:,0,0,d])
     k_w=np.size(kernel[0,:,0,d])
     k_d=np.size(kernel[0,0,:,d])
     if(padding=="valid"):
-        
+
         l=int((img_length-k_l)/stride)+1
         w=int((img_width-k_w)/stride)+1
-        
+
         pad_length=k_l-(img_length-((l-1)*stride+k_l))
         pad_width=k_w-(img_width-((w-1)*stride+k_w))
-        
+
         img_padded=np.zeros((img_length+pad_length,img_width+pad_width,img_depth));
         img_padded[:-pad_length,:-pad_width,:]=img
-        
-        
+
+
         img_padded_width=np.size(img_padded[0,:,0])
         img_padded_length=np.size(img_padded[:,0,0])
-        img_padded_depth=np.size(img_padded[0,0,:]) 
-        
-        
+        img_padded_depth=np.size(img_padded[0,0,:])
+
+
         l=int((img_padded_length-k_l)/stride)+1
         w=int((img_padded_width-k_w)/stride)+1
         output=np.zeros((l,w))
         for i in range(l):
             for j in range(w):
-                 output[i,j]=activation_function(correlation(img_padded,i*stride,j*stride,kernel,d)+bias[d,0]) 
-                
-                
+                 output[i,j]=activation_function(correlation(img_padded,i*stride,j*stride,kernel,d)+bias[d,0])
+
+
     if(padding=="same"):
         required_length=(img_length-1)*stride+k_l
         required_width=(img_width-1)*stride+k_w
@@ -68,25 +68,25 @@ def convolution(img,kernel,d,padding,stride,bias):
         r_w=int(required_padd_width/2)
         img_padded=np.zeros((required_length,required_width,img_depth))
         img_padded[r_l:r_l+img_length,r_w:r_w+img_width,:]=img
-     
+
         img_padded_width=np.size(img_padded[0,:,0])
         img_padded_length=np.size(img_padded[:,0,0])
-        img_padded_depth=np.size(img_padded[0,0,:]) 
-        
+        img_padded_depth=np.size(img_padded[0,0,:])
+
         l=int((img_padded_length-k_l)/stride)+1
         w=int((img_padded_width-k_w)/stride)+1
         output=np.zeros((l,w))
         for i in range(l):
             for j in range(w):
-                output[i,j]=activation_function(correlation(img_padded,i*stride,j*stride,kernel,d)+bias[d,0]) 
-                
-                
+                output[i,j]=activation_function(correlation(img_padded,i*stride,j*stride,kernel,d)+bias[d,0])
+
+
     return output
 
 def convolution_util(img,kernels,stride,padding,activation_function):
     depth=np.size(kernels[0,0,0,:]);
     bias1=np.zeros((depth,1))
-    
+
     for i in range(depth):
         if(i==0):
              out=convolution(img,kernels,i,"valid",stride,bias1)
@@ -94,16 +94,16 @@ def convolution_util(img,kernels,stride,padding,activation_function):
              m=np.size(out[0,:])
              output=np.zeros((n,m,depth))
              output[:,:,i]=out
-        
+
         output[:,:,i]=convolution(img,kernels,i,"valid",stride,bias1)
     return output
-        
+
 
 #plt.imshow(img)
 img = cv2.imread('image1.jpg')
 #plt.imshow(img)
 
-               
+
 depth=np.size(img[0,0,:]);
 kernel= np.zeros((10,10,depth,3))
 k_l=np.size(kernel[:,0,0,0])
@@ -115,13 +115,10 @@ for i in range(k_l):
         for k in range(depth):
             for l in range(number_filter):
                 kernel[i,j,k,l]= np.random.normal(0, 1, 1)
-                 
-                 
-   
+
+
+
 test=convolution_util(img,kernel,5,"valid",activation_function)
 
 plt.imshow(test[:,:,2], cmap = 'gray')
 plt.show()
-            
-       
-    
